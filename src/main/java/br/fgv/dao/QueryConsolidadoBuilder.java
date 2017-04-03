052202d1d54f80ab2d67d1d4985befbbeb147d20
Luiz Gabriel <luizgabriel.info@gmail.com>
2017-04-03 00:25:33 -0300

    Fixed zona eleitoral

--- src/main/java/br/fgv/dao/QueryConsolidadoBuilder.java
+++ src/main/java/br/fgv/dao/QueryConsolidadoBuilder.java
@@ -19,20 +19,33 @@ public class QueryConsolidadoBuilder {
 
     public String build() {
         String reg = args.getNivelRegional().getCamposAgregar();
-        if (args.getNivelRegional().equals(AgregacaoRegional.MUNICIPIO)) reg = "cod"; //Instead of mun_cod
 
-        qb.select_()
-                .comma("m." + reg, "mc.cargo")
-                .comma_().sum("mc.qtd_aptos")._as_().valor("eleitores_aptos")
-                .comma_().sum("(mc.qt_votos_legenda + mc.qt_votos_nominais)")._as_().valor("votos_validos")
-                .comma_().sum("(((mc.qt_votos_legenda + mc.qt_votos_nominais) + mc.qt_votos_brancos) + mc.qt_votos_nulos)")._as_().valor("votos_totais_consolidado")
-                .comma_().sum("mc.qt_votos_brancos")._as_().valor("votos_brancos")
-                .comma_().sum("mc.qt_votos_nulos")._as_().valor("votos_nulos")
+        if (args.getNivelRegional().equals(AgregacaoRegional.MUNICIPIO))
+            reg = "cod"; //Instead of mun_cod
+
+
+        qb.select_();
+
+        if (args.getNivelRegional().equals(AgregacaoRegional.UF_ZONA))
+            qb.comma("m.uf", "mc.zona", "mc.cargo");
+        else
+            qb.comma("m." + reg, "mc.cargo");
+
+        qb
+            .comma_().sum("mc.qtd_aptos")._as_().valor("eleitores_aptos")
+            .comma_().sum("(mc.qt_votos_legenda + mc.qt_votos_nominais)")._as_().valor("votos_validos")
+            .comma_().sum("(((mc.qt_votos_legenda + mc.qt_votos_nominais) + mc.qt_votos_brancos) + mc.qt_votos_nulos)")._as_().valor("votos_totais_consolidado")
+            .comma_().sum("mc.qt_votos_brancos")._as_().valor("votos_brancos")
+            .comma_().sum("mc.qt_votos_nulos")._as_().valor("votos_nulos")
 
             ._from_().comma("munzona_detalhe_consolidado as mc", "aux_municipio as m")
                 ._where_().eq("m.cod", "mc.codigo_municipio")
-                ._and_().eq("mc.ano", ano)
-                ._group_by_().comma("m." + reg, "mc.cargo");
+                    ._and_().eq("mc.ano", ano);
+
+        if (args.getNivelRegional().equals(AgregacaoRegional.UF_ZONA))
+            qb._group_by_().comma("m.uf", "mc.zona", "mc.cargo");
+        else
+            qb._group_by_().comma("m." + reg, "mc.cargo");
 
         return qb.toString(ano);
     }
--- src/main/java/br/fgv/dao/QueryFatoConsolidadoBuilder.java
+++ src/main/java/br/fgv/dao/QueryFatoConsolidadoBuilder.java
@@ -10,7 +10,7 @@ public class QueryFatoConsolidadoBuilder {
     private final String ano;
     private final String queryFato;
     private final String queryConsolidado;
-    private final QueryBuilder queryBuilder = new QueryBuilder();
+    private final QueryBuilder qb = new QueryBuilder();
 
 
     public QueryFatoConsolidadoBuilder(ArgumentosBusca args, String ano, String queryFato, String queryConsolidado) {
@@ -27,15 +27,19 @@ public class QueryFatoConsolidadoBuilder {
         if (args.getNivelRegional().equals(AgregacaoRegional.MUNICIPIO))
             reg_c = "cod"; //Instead of mun_cod
 
-        queryBuilder
-                .select_().comma("mun.*", "c.eleitores_aptos", "c.votos_validos", "c.votos_totais_consolidado", "c.votos_brancos", "c.votos_nulos")
-                ._from_().par(queryFato)._as_().valor("mun")
-                ._left_join_().par(queryConsolidado)._as_().valor("c")
-                ._on_()
-                    .eq("c." + reg_c, "mun." + reg)
-                    ._and_()
-                    .eq("c.cargo", args.getFiltroCargo());
+        qb.select_().comma("mun.*", "c.eleitores_aptos", "c.votos_validos", "c.votos_totais_consolidado", "c.votos_brancos", "c.votos_nulos")
+            ._from_().par(queryFato)._as_().valor("mun")
+            ._left_join_().par(queryConsolidado)._as_().valor("c")
+            ._on_();
 
-        return queryBuilder.toString(ano);
+
+        if (args.getNivelRegional().equals(AgregacaoRegional.UF_ZONA))
+            qb.eq("c.zona", "mun.zona")._and_().eq("c.uf", "mun.uf");
+        else
+            qb.eq("c." + reg_c, "mun." + reg);
+
+        qb._and_().eq("c.cargo", args.getFiltroCargo());
+
+        return qb.toString(ano);
     }
 }
